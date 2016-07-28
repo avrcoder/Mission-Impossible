eaa2dc5471d4952134f93bace278ac8df0d6bccc
Vishwatej Reddy <avrcoder11@gmail.com>
2016-07-28 13:03:01 +0530

    Create Delete_any_pointer.py

---
+++ Data-structures/Linked-List/Delete_any_pointer.py
@@ -0,0 +1,50 @@
+class LinkedList(object):
+	def __init__(self):
+		self.head = None
+		self.second = None
+
+	class Node(object):
+		def __init__(self,data):
+			self.data = data
+			self.next = None
+
+	def delete(self,node):
+		temp = node.next
+		node.next = temp.next
+		node.data = temp.data
+		temp = None
+
+	def getcount(self):
+		c = 0
+		temp = self.head
+		while(temp!=None):
+			temp = temp.next
+			c+=1
+		return c
+
+	def printlist(self):
+		temp = self.head
+		while(temp!=None):
+			print temp.data,
+			temp = temp.next
+		print
+
+	def push(self, new_data):
+		new_Node = self.Node(new_data)
+		new_Node.next = self.head
+		self.head = new_Node
+ 
+
+llist = LinkedList()
+llist.push(1)
+llist.push(2)
+llist.push(3)
+llist.push(4)
+llist.push(5)
+llist.push(6)
+llist.push(7)
+llist.printlist()
+llist.delete(llist.head)
+llist.printlist()
+# llist.printlist()
+# res = llist.merge(llist)
